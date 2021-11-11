00823522cd304812d693330c6b35c3737f9408a1
DaoistDuck <dragonoath123@gmail.com>
2021-11-10 19:58:09 -0800

    Found Best Parameters for Max Depth and Max Leaf
    What I did was train the model twice, but with different parameters.
    First time, just wanted to find best max_depth.
    Second time, I used value for max_depth and wanted to find best max_leaf

--- mnist-mini-forest.py
+++ mnist-mini-forest.py
@@ -1,9 +1,7 @@
 import scipy.io
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
-from sympy import plot
 
 mat = scipy.io.loadmat("MNISTmini.mat")
 
@@ -23,13 +21,6 @@ c1_idx = c1_idx[0, 0:1500]
 c2_idx = np.array(c2_idx)
 c2_idx = c2_idx[0, 0:1500]
 
-# Concatenate arrays and perform random permutation
-# all_idx = np.array([])
-# for i in range(500, 2000, 500):
-#     temp1 = c1_idx[i-500:i] #i = 500,  0-499, i=1000, 500-999,
-#     temp2 = c2_idx[i-500:i]
-#     all_idx = np.concatenate([temp1, temp2])
-# all_idx = np.random.permutation(all_idx)
 
 # train/val/test split
 train_idx = np.concatenate([c1_idx[:500], c2_idx[:500]])  # all_idx[0:1000]
@@ -42,48 +33,88 @@ test_idx = np.concatenate(
 # x_train: digits, y_train: labels
 x_train = data_fea[train_idx, :]
 y_train = data_gnd[train_idx, :]
-plotx = []
-ploty1 = []
-ploty = []
-for i in range(2, 50):  # max_leaf_nodes must be higher than 1, so i just started at 2
-    # logregr model initialization and training
-    clf = RandomForestClassifier(
-        n_estimators=1, bootstrap=False, random_state=0)
-    clf.fit(x_train, y_train.ravel())
 
+plotI = []
+plotMaxDepthVal = []
+plotMaxDepthTrain = []
+for i in range(1, 1000):
+    # random forest model initialization and training
+    maxDepth = RandomForestClassifier(
+        n_estimators=1, bootstrap=False, random_state=0, max_depth = i)
+    maxDepth.fit(x_train, y_train.ravel())
+    
     # Make predictions
-    y_pred = clf.predict(data_fea[validation_idx, :])
+    maxDepthPred = maxDepth.predict(data_fea[validation_idx, :])
 
     # Assess performance
-    val_score = clf.score(
+    maxDepth_val_score = maxDepth.score(
         data_fea[validation_idx, :], data_gnd[validation_idx, :])
-    train_score = clf.score(data_fea[train_idx, :], data_gnd[train_idx, :])
-    ploty.append(val_score)
-    ploty1.append(train_score)
-    plotx.append(i)
+    maxDepth_train_score = maxDepth.score(data_fea[train_idx, :], data_gnd[train_idx, :])
 
-df = pd.DataFrame()
-df["x"] = plotx
-df["val_score"] = ploty
-df["train_score"] = ploty1
-maxVal = df["val_score"]
-maxValidx = maxVal.idxmax()
-maxTrain = df["train_score"]
-maxTrainidx = maxTrain.idxmax()
-x = plotx[maxValidx]
-maxVal = ploty[maxValidx]
 
-# ax = plt.gca()
-# df.plot(kind="line", x="x", y="val_score", color="blue", ax=ax)
-# df.plot(kind="line", x="x", y="train_score", color="red", ax=ax)
-fig = plt.figure()
-plt.plot(plotx, ploty, color="blue", label="Val_Score")
-plt.plot(plotx, ploty1, color="red", label="Train_Score")
-plt.scatter(x, maxVal, color="black", zorder=1, label="Max Val_Score")
+    # Push the performace and value of C into list
+    plotMaxDepthVal.append(maxDepth_val_score)
+    plotMaxDepthTrain.append(maxDepth_train_score)
+    plotI.append(i)
+
+
+# Find (x,y) of best validation score
+maxDepthVal = max(plotMaxDepthVal)
+maxDepthValidx = plotMaxDepthVal.index(maxDepthVal)
+maxplotIidx = plotI[maxDepthValidx]
+print(maxplotIidx)
+print(maxDepthVal)
+
+# Plot the Score of both validation and training
+plt.figure(1)
+plt.semilogx(plotI, plotMaxDepthVal, color="blue", label="Val_Score")
+plt.semilogx(plotI, plotMaxDepthTrain, color="red", label="Train_Score")
 plt.legend(loc="lower right")
+plt.text(maxplotIidx, maxDepthVal, ' {} , {}'.format(maxplotIidx, maxDepthVal))
+plt.xlabel("Max Depth Value Increasing Continously")
+plt.ylabel("Score Values")
 
-plt.text(x, maxVal, '  {} , {}'.format(x, maxVal))
-# fig, ax1 = plt.subplots()
-# ax1.plot(x, maxVal, "go", label="marker only")
+# Show Graph
+plt.show()
 
+plotI2 = []
+plotMaxLeafVal = []
+plotMaxLeafTrain = []
+for i in range(2,1000):
+    # random forest model initialization and training
+    maxLeaf = RandomForestClassifier(
+            n_estimators=1, bootstrap=False, random_state=0, max_depth = maxplotIidx, max_leaf_nodes = i+1)
+    maxLeaf.fit(x_train, y_train.ravel())
+
+    # Make predictions
+    maxLeafPred = maxLeaf.predict(data_fea[validation_idx, :])
+
+    # Assess performance
+    maxLeaf_val_score = maxLeaf.score(
+        data_fea[validation_idx, :], data_gnd[validation_idx, :])
+    maxLeaf_train_score = maxLeaf.score(data_fea[train_idx, :], data_gnd[train_idx, :])
+
+    # Push the performace and value of C into list
+    plotMaxLeafVal.append(maxLeaf_val_score)
+    plotMaxLeafTrain.append(maxLeaf_train_score)
+    plotI2.append(i)
+
+
+# Find (x,y) of best validation score
+maxLeafVal = max(plotMaxLeafVal)
+maxLeafValidx = plotMaxLeafVal.index(maxLeafVal)
+maxplotI2idx = plotI2[maxLeafValidx]
+print(maxplotI2idx)
+print(maxLeafVal)
+
+# Plot the Score of both validation and training
+plt.figure(2)
+plt.semilogx(plotI2, plotMaxLeafVal, color="blue", label="Val_Score")
+plt.semilogx(plotI2, plotMaxLeafTrain, color="red", label="Train_Score")
+plt.legend(loc="lower right")
+plt.text(maxplotI2idx, maxLeafVal, ' {} , {}'.format(maxplotIidx, maxLeafVal))
+plt.xlabel("Max Leaf Node Value Increasing Continously")
+plt.ylabel("Score Values")
+
+# Show Graph
 plt.show()
